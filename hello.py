19bd8dda9ed79fa6d57876baff9678202950bbb9
Aaron Huang <ahuang25@andover.edu>
2025-04-09 12:12:30 -0400

    begin italian brainrot

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
+import 
+
 print("hello")
+class Italian_Brainrot:
+    def __init__(self, name, image, description):
+        self.name = name
+        self.image = image
+        self.description = description
+    
+    def generate_image(self):
+        
+
+    def 
